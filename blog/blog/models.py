549a06464a8f78de90ac3e1ac4e7c94b4484d364
Sam Hsu <hsuh@email.sc.edu>
2023-07-09 21:13:09 -0400

    blog app - static file

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 
 class Post(models.Model):
+    
     title = models.CharField(max_length=200)
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE,)
-    
     body = models.TextField()
     
     def __str__(self):
