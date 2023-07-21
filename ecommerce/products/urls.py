971dfcc3b1b0d0a52cb8ec77005c349050721f67
Sam Hsu <hsuh@email.sc.edu>
2023-07-20 22:10:14 -0400

    revise

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import(
-    ProductDetailView, 
+from .views import (
+    ProductDetailView,
     ProductListView,
     add_to_cart,
-    remove_single_product_from_cart
-    
-    )
+    remove_single_product_from_cart,
+)
 
 urlpatterns = [
     path("<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("add-to-cart/<int:pk>/", add_to_cart, name='add_to_cart'),
-    # path("remove-from-cart/<int:pk>/", remove_single_product_from_cart),
-    path("", ProductListView.as_view(), name="product_list")
+    path("remove-from-cart/<int:pk>/", remove_single_product_from_cart, name='remove_from_cart'),
+    path("", ProductListView.as_view(), name="product_list"),
 ]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, ListView
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404
 from .models import Product, OrderProduct, Order
-from django.shortcuts import redirect
 from django.utils import timezone
 
 
 class ProductListView(ListView):
     model = Product
     template_name = "product_list.html"
-    
+
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = "product.html"
-    
+
+
 def add_to_cart(request, pk):
     product = get_object_or_404(Product, pk=pk)
     user = request.user
@@ -25,63 +25,54 @@ def add_to_cart(request, pk):
         ordered=False
     )
     order_qs = Order.objects.filter(
-    user=request.user,
-    ordered=False
+        user=request.user,
+        ordered=False
     )
     
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
-        # check if the order product is in the order
-        if order.products.filter(product__pk = product.pk).exists():
+        if order.products.filter(product__pk=product.pk).exists():
             order_product.quantity += 1
             order_product.save()
             messages.info(request, "This product quantity was updated.")
-            return redirect("product_detail", pk=pk)
         else:
             order.products.add(order_product)
             messages.info(request, "This product was added to your cart.")
-            return redirect("product_detail", pk=pk)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
-            user=request.user, ordered_date=ordered_date)
+            user=request.user,
+            ordered_date=ordered_date
+        )
         order.products.add(order_product)
         messages.info(request, "This product was added to your cart.")
-        return redirect("product_detail", pk=pk)
     
+    return redirect("product_detail", pk=pk)
+
+
 def remove_single_product_from_cart(request, pk):
-    product = get_object_or_404(product, pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     order_qs = Order.objects.filter(
         user=request.user,
         ordered=False
     )
     if order_qs.exists():
         order = order_qs[0]
-        # check if the order product is in the order
         if order.products.filter(product__pk=product.pk).exists():
-            order_product = OrderProduct.objects.filter(
+            order_product = OrderProduct.objects.get(
                 product=product,
                 user=request.user,
                 ordered=False
-            )[0]
+            )
             if order_product.quantity > 1:
                 order_product.quantity -= 1
                 order_product.save()
             else:
-                order.items.remove(order_product)
+                order.products.remove(order_product)
             messages.info(request, "This item quantity was updated.")
-            return redirect("product_detail", pk=pk)
         else:
             messages.info(request, "This item was not in your cart")
-            return redirect("product_detail", pk=pk)
     else:
         messages.info(request, "You do not have an active order")
-        return redirect("product_detail", pk=pk)
-        
-            
-            
-            
-
-
-
+    
+    return redirect("product_detail", pk=pk)
